c9eb2fb824af6efc6b7b136e81faa2d2519d49dd
Lincoln Perez <126333617+LincolnEPP1998@users.noreply.github.com>
2023-06-03 11:07:09 -0500

    Update Librerias.py

--- VIDEO/Librerias.py
+++ VIDEO/Librerias.py
@@ -1,1 +1,23 @@
+# Por entra un documento zip
+!pip install remotezip tqdm opencv-python einops
+# Install TensorFlow 2.10
+!pip install tensorflow==2.10.0 # Modelos Machine Learning (ML)
+!pip install ultralytics # Detección de objetos
+!pip install opencv-python-headless # carga de datos
+!pip install opencv-python # carga de datos
+!pip install tensorflow gradio # Modelos de ML
+!pip install tensorflow_hub # Modelos de Ml
+!pip install Pillow # Manipulacion de imagenes
+!pip install matplotlib # visualizacion 
+!pip install gradio # Modelos Ml
+!pip install torch torchvision torchaudio # Extraccion de datos 
+!pip install easyocr # Extraccion de datos
+
+
+# librerias necesarias
+
+import cv2 # Descargar documento CSV
+import numpy as np # Manipulación de Array y calculos matematicos
+from PIL import Image # Manipulacion de imagenes
+import easyocr # Manipulacion de texto sobre imagenes
 
