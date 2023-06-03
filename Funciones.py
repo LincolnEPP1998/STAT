06c4d97951362314d287b98861253fe92c8b9c7d
Lincoln Perez <126333617+LincolnEPP1998@users.noreply.github.com>
2023-06-03 08:30:45 -0500

    Update Funciones.py

--- Funciones.py
+++ Funciones.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+def TEXTO(path):
+  try:
+    with pandas.open(path) as file:
+      text=''
+      for page in file:
+        text += page.getText()
+        print(text)
+        except:
+          pass
